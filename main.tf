087ccea85643056140963acf6ef03a830fe0afc2
hemarajspring <81808944+hemarajspring@users.noreply.github.com>
2022-08-25 22:37:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
-
+resource "aws_cloudwatch_log_group" "main" {
+  name = "clg-${var.region_id}-${var.environment}-${var.owner}-${var.application_service}-${var.seq_id}"
+  retention_in_days = var.retention_in_days
+  
+  tags = merge(
+    local.tags,
+    tomap{{ "Name"=clg-${var.region_id}-${var.environment}-${var.owner}-${var.application_service}-${var.seq_id}"})
+    )
+}
