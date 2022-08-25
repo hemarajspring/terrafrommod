26051269512c36cc85f9bff3b0cfea6660879a7b
hemarajspring <81808944+hemarajspring@users.noreply.github.com>
2022-08-25 22:44:47 +0530

    Create local.tf

---
+++ local.tf
@@ -0,0 +1,19 @@
+locals {
+  tags = {
+    version  = var.seq_id
+    cluster = var.cluster_member
+    applicationservice = var.application_service
+    applicationrole = var.application_role
+    environment = var.environment
+    builddate = var.build_date
+    maintenanceday = var.maintenance_day
+    maintenancetime = var.maintenance_time
+    owner = var.owner
+    project = var.project
+    region = var.region
+    confidentiality = var.confidentiality
+    compliance = var.compliance
+    businessunit =var.business_unit
+    managedby = "terraform"
+  }
+}
