b936998f7085c1b702a49dc0e19b08fed84af363
hemarajspring <81808944+hemarajspring@users.noreply.github.com>
2022-11-02 11:35:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_cloudwatch_log_group" "main" {
   #name = "clg-${var.region_id}-${var.environment}-${var.owner}-${var.application_service}-${var.seq_id}"
   retention_in_days = var.retention_in_days
   
-  tags = merge(
-    local.tags,
-    tomap ({"name" = "clg-${var.region_id}-${var.environment}-${var.owner}-${var.application_service}-${var.seq_id}"})
-  )
+  #tags = merge(
+   # local.tags,
+    #tomap ({"name" = "clg-${var.region_id}-${var.environment}-${var.owner}-${var.application_service}-${var.seq_id}"})
+  #)
 }
